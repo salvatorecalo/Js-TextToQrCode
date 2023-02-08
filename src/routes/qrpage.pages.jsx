8fcc92261f850c687d334fa20895ffbdefe38ebf
Salvo <dafder45678@gmail.com>
2023-02-08 19:22:45 +0100

    fixed shown component qr

--- src/routes/qrpage.pages.jsx
+++ src/routes/qrpage.pages.jsx
@@ -1,28 +1,23 @@
-import {useRef, useEffect} from 'react';
+import {useRef, useState} from 'react';
 
 
 export default function QrPage() {
     const ref = useRef(null);
-
-    useEffect(() => {
-        const element = ref.current;
-        console.log(element);
-        console.log(element.id);
-      }, []);
-
+    const [shown, SetShown] = useState(false)
     function generateQr() {
             const element = ref.current;
             element.style.display = "block";
             let QrText = document.getElementById("text").value;
             if (QrText.trim().length === 0) {
-                console.log("fired")
-
+                SetShown(false)
                 document.getElementById("error").innerHTML = "please enter some text!";
                 document.getElementById("img").style.display = "none";
             } else {
+                SetShown(true)
                 document.getElementById("img").style.display = "block";
                 document.getElementById("error").innerHTML = "";
                 document.getElementById("img").src = "https://api.qrserver.com/v1/create-qr-code/?size=240x240&data=" + QrText;
+                console.log(document.getElementById("img").src)
             }
         }
 
@@ -31,11 +26,11 @@ export default function QrPage() {
             <h1 className="text-4xl font-medium mb-4">Qr Code Generator</h1>
             <div id="qr-image" ref={ref}>
                 <span id="error" className='text-red-500'></span>
-                <img src="" id="img" alt="generated qrcode" style={{"display": "hidden"}}/>
+                {shown && <img src="" id="img" alt="generated qrcode" />}
             </div>
             <input type="text" id="text" className="border-2 rounded-lg text-xl my-8 py-2 px-4" placeholder="Enter your text..."/>
             <button onClick={generateQr} className="bg-emerald-500 hover:bg-emerald-600 py-2 px-6 text-white">Genera Qr</button>
-
+            {shown && <a className='mt-8' href="">Scarica Qr Code</a>}
             <a href={`/`} className='mt-24 text-lg font-medium'>Torna alla home</a>
         </div>
     );
