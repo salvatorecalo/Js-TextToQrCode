06a1eb86d00e468f6554e6df603ea4d9171b17f6
Salvo <dafder45678@gmail.com>
2023-02-11 15:54:50 +0100

    sistemati link di condivisone social

--- src/components/SocialMedia/socialMedia.component.jsx
+++ src/components/SocialMedia/socialMedia.component.jsx
@@ -1,6 +1,5 @@
 import {
     faTwitter,
-    faFacebook,
     faWhatsapp,
     faTelegram
   } from "@fortawesome/free-brands-svg-icons";
@@ -9,24 +8,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function SocialFollow() {
   return (
     <div className="flex justify-center items-center">
-        <a href="mailto:email@example.com?&subject=Avviso%20%importante%20%&body=Ho%20%scoperto%20%questo%20%fantastico%20%sito%20%che%20%mi%20%permette%20%di%20%convertire%20%testo%20%e%20%link%20%in%20%Qrcode%20%js-text-to-qr-code.vercel.app"
+        <a href="mailto:email@example.com?&subject=Avviso importante&body=Ho scoperto questo fantastico sito che mi permette di convertire testo e link in Qrcode https://js-text-to-qr-code.vercel.app"
         className="envelope social mr-4"
         target="_blank">
         <FontAwesomeIcon icon={faEnvelope} size="2x" />
       </a>
-      <a href="https://twitter.com/intent/tweet?text=Ho%20%scoperto%20%questo%20%fantastico%20%sito%20%che%20%mi%20%permette%20%di%20%convertire%20%testo%20%e%20%link%20%in%20%Qrcode%20%js-text-to-qr-code.vercel.app" className="twitter  twitter-share-button social mr-4" target="_blank">
+      <a href="https://twitter.com/intent/tweet?text=Ho%20scoperto%20questo%20fantastico%20sito%20che%20mi%20permette%20di%20convertire%20testo%20e%20link%20in%20Qrcode%20https://js-text-to-qr-code.vercel.app"  className="twitter-share-button social mr-4" target="_blank">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
-      <a href="https://www.instagram.com/learnbuildteach"
-        className="facebook social mr-4">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </a>
-      <a href="https://t.me/share/share?url=Ho%20%scoperto%20%questo%20%fantastico%20%sito%20%che%20%mi%20%permette%20%di%20%convertire%20%testo%20%e%20%link%20%in%20%Qrcode%20%js-text-to-qr-code.vercel.app"
+      <a href="https://t.me/share/share?url=https://js-text-to-qr-code.vercel.app&text=Ho scoperto questo fantastico sito che mi permette di convertire testo e link in Qrcode js-text-to-qr-code.vercel.app"
         className="telegram social mr-4"
         target="_blank">
         <FontAwesomeIcon icon={faTelegram} size="2x" />
       </a>
-      <a href="https://api.whatsapp.com/send?text=Ciao+ho+scoperto+questo+fantastico+sito+che+mi+permette+di+convertire+testo+e+link+in+un+qrcode+js-text-to-qr-code.vercel.app"
+      <a href="https://api.whatsapp.com/send?text=Ho scoperto questo fantastico sito che mi permette di convertire testo e link in Qrcode js-text-to-qr-code.vercel.app https://js-text-to-qr-code.vercel.app"
         className="whatsapp social mr-4" target="_blank">
         <FontAwesomeIcon icon={faWhatsapp} size="2x" />
       </a>
